86cf106dd70541e490cf5d0f523c173a69060cb8
Toni Langley <chibia2@gmail.com>
2017-03-27 14:36:49 -0400

    Update from challenge-template and browser-template
    Refresh for cohort lm01

--- grunt/webpack-dev-server.js
+++ grunt/webpack-dev-server.js
@@ -1,23 +1,32 @@
-'use strict';
+'use strict'
 
-let clone = require('clone');
+const clone = require('clone')
 
 // clone the webpack config to separate configuration of webpack and dev server
-let webpackConfig = clone(require('./webpack').options);
+const webpackConfig = clone(require('./webpack').options)
 
-// enable live reload without a script tag
-webpackConfig.entry.vendor.unshift('webpack-dev-server/client?http://localhost:8080');
+// port for development server
+const port = +('GA'.split('').reduce((p, c) => p + c.charCodeAt(), ''))
+
+// make `jQuery` and `$` available in the development console
+webpackConfig.module.loaders.push({
+  test: require.resolve('jquery'),
+  loader: 'expose?jQuery!expose?$'
+})
 
 module.exports = {
   options: {
+    port,
+    inline: true, // reload on change
     webpack: webpackConfig,
+    publicPath: '/public/'
   },
 
   start: {
     keepAlive: true,
     webpack: {
       devtool: 'source-map',
-      debug: 'true',
-    },
-  },
-};
+      debug: 'true'
+    }
+  }
+}
